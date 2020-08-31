19351882daacbeae21bb13a08e27aafb8935d1a3
Jingcheng Su <jingchengsu@Jingchengs-MacBook-Air.local>
2020-08-31 17:07:11 +0800

    minor change

--- app.js
+++ app.js
@@ -15,12 +15,44 @@
 //   console.log(`Server running at http://${hostname}:${port}/`);
 // });
 
-const Koa = require('koa');
-const app = new Koa();
+
+// koa hello world application
+
+// const Koa = require('koa');
+// const app = new Koa();
  
-// response
-app.use(ctx => {
-  ctx.body = 'Hello world';
+// // response
+// app.use(ctx => {
+//   ctx.body = 'Hello world';
+// });
+ 
+// app.listen(3000);
+
+
+
+
+
+
+const rp = require('request-promise');
+const requestOptions = {
+  method: 'GET',
+  uri: 'https://undefined/v1/cryptocurrency/quotes/latest/symbol=BTC',
+  qs: {
+    'start': '1',
+    'limit': '5000',
+    'convert': 'USD'
+  },
+  headers: {
+    'X-CMC_PRO_API_KEY': API_KEY
+  },
+  json: true,
+  gzip: true
+};
+
+
+
+rp(requestOptions).then(response => {
+  console.log('API call response:', response);
+}).catch((err) => {
+  console.log('API call error:', err.message);
 });
- 
-app.listen(3000);
--- index.js
+++ index.js
@@ -4,13 +4,8 @@ var koa = require('koa');
 
 var app = new koa();
 
-//Set up body parsing middleware
-
-
-//Require the Router we defined in movies.js
 var price = require('./helper.js');
 
-//Use the Router on the sub route /movies
 app.use(price.routes());
 
 app.listen(3000);
