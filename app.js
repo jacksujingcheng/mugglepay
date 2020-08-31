ca0b3bf2577076ae5e93a038f32740bf2a90b572
Jingcheng Su <jingchengsu@Jingchengs-MacBook-Air.local>
2020-08-31 17:08:32 +0800

    minor change

--- app.js
+++ app.js
@@ -27,32 +27,3 @@
 // });
  
 // app.listen(3000);
-
-
-
-
-
-
-const rp = require('request-promise');
-const requestOptions = {
-  method: 'GET',
-  uri: 'https://undefined/v1/cryptocurrency/quotes/latest/symbol=BTC',
-  qs: {
-    'start': '1',
-    'limit': '5000',
-    'convert': 'USD'
-  },
-  headers: {
-    'X-CMC_PRO_API_KEY': API_KEY
-  },
-  json: true,
-  gzip: true
-};
-
-
-
-rp(requestOptions).then(response => {
-  console.log('API call response:', response);
-}).catch((err) => {
-  console.log('API call error:', err.message);
-});
